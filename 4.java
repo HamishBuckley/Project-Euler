d4666794141496f838b167bc14ad3b6dd5f66638
HamishBuckley <hamishbuckley@gmail.com>
2016-08-24 11:53:18 +1200

    thoughts

--- 4.java
+++ 4.java
@@ -6,3 +6,8 @@ public static void main(String[] args){
 public static int LargestPalindromeProduc(int inputnumber){
 	
 }
+int = 9
+if intlength== 1 
+return 9
+for I=10; I = intlength x 10 ; i x=10
+int += 9 x i
