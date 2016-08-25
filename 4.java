41e88f993076a63c0b49a87b18b6b8a549b4f7c0
HamishBuckley <hamishbuckley@gmail.com>
2016-08-25 15:34:10 +1200

    Isn't finding the largest number, I need to check for factors from the largest prime down

--- 4.java
+++ 4.java
@@ -1,13 +1,38 @@
-999 x 999 = 998001
-public static void main(String[] args){
-	
-}
+package main;
 
-public static int LargestPalindromeProduc(int inputnumber){
+import  java.lang.StringBuilder;
+
+public class MAIN {
+
+	public static void main(String[] args){
+		System.out.print(PalindromeFactorFinder(1));
+	}
 	
+	private static String PalindromeFactorFinder(int input) {
+		while (true){
+			for (int i = 999; i > 100; i--){
+				for (int j = 999; j > 100; j--){
+					System.out.println(i);
+					if (IsPalindrome(j * i)){
+						return Integer.toString(j) + " " + Integer.toString(i);
+					}
+				}
+			}
+		}
+	}
+	private static boolean IsPalindrome(int inputnumber) {
+		String inputstring = String.valueOf(inputnumber);
+		if (inputstring.length() % 2 == 0){
+			
+			if ((inputstring.substring(0 , inputstring.length() / 2)).equals((new StringBuilder(inputstring.substring(inputstring.length() / 2, (inputstring.length()))).reverse().toString()))) {
+				return true;
+			}
+		}
+		else{
+			if (inputstring.substring(0 , (inputstring.length() / 2)).equals(new StringBuilder(inputstring.substring((inputstring.length() / 2) + 1, (inputstring.length()))).reverse().toString())){
+				return true;
+		}
+		}
+		return false;
 }
-int = 9
-if intlength== 1 
-return 9
-for I=10; I = intlength x 10 ; i x=10
-int += 9 x i
+}
